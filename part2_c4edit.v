0c11cc133abc9adc0256f798b9fca1ae240a7498
joouellette <joseph.ouellette@hotmail.com>
2016-03-27 16:32:00 -0400

    further commenting before diving in
    this iteration precedes the implementation of control and datapath, who
    probably need a bit of high level planning first

--- part2_c4edit.v
+++ part2_c4edit.v
@@ -22,8 +22,8 @@ module part2
 	);
 
 	input			CLOCK_50;				//	50 MHz
-	input   [9:0]   SW;
-	input   [3:0]   KEY;
+	input   [9:0]   SW; //as was said, we could duplicate these (and not call them switches) for our position
+	input   [3:0]   KEY; //will probably have to be split into various inputs
 
 	// Declare your inputs and outputs here
 	// Do not change the following outputs
@@ -40,10 +40,10 @@ module part2
 	assign resetn = KEY[0];
 	
 	// Create the colour, x, y and writeEn wires that are inputs to the controller.
-	wire [2:0] colour;
-	wire [7:0] x;
+	wire [2:0] colour; //colour is like the last thing we care about
+	wire [7:0] x; //almost the correct size already! jk 7 is only 3 bits long
 	wire [6:0] y;
-	wire writeEn;
+	wire writeEn; //this is almost for sure useless but I haven't delved in enough to say why
 
 	// Create an Instance of a VGA controller - there can be only one!
 	// Define the number of colours as well as the initial background
@@ -54,7 +54,9 @@ module part2
 			.colour(colour),
 			.x(x),
 			.y(y),
-			.plot(writeEn),
+			.plot(writeEn),//Right, so we need a counter that will control this things value while
+						  //while also changing the initial x, y positions slightly to fill in values
+						 //could an FSM control all of that power???
 			/* Signals for the DAC to drive the monitor. */
 			.VGA_R(VGA_R),
 			.VGA_G(VGA_G),
@@ -64,10 +66,10 @@ module part2
 			.VGA_BLANK(VGA_BLANK),
 			.VGA_SYNC(VGA_SYNC),
 			.VGA_CLK(VGA_CLK));
-		defparam VGA.RESOLUTION = "160x120";
+		defparam VGA.RESOLUTION = "160x120"; //is this what we want?
 		defparam VGA.MONOCHROME = "FALSE";
 		defparam VGA.BITS_PER_COLOUR_CHANNEL = 1;
-		defparam VGA.BACKGROUND_IMAGE = "black.mif";
+		defparam VGA.BACKGROUND_IMAGE = "black.mif"; //this should be background.mif
 			
 	// Put your code here. Your code should produce signals x,y,colour and writeEn/plot
 	// for the VGA controller, in addition to any other functionality your design may require.
